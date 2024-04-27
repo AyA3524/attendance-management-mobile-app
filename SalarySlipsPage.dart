78c2be044f63fa69f6981496e1e4469e7fc75220
AyA <zer.aya.nh@gmail.com>
2024-04-27 18:17:21 +0100

    Create SalarySlipsPage.dart

---
+++ SalarySlipsPage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class SalarySlipsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Salary Slips'),
+      ),
+      body: Center(
+        child: Text('Salary Slips Page'),
+      ),
+    );
+  }
+}
